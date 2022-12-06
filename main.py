4a9cbfb99a6d0722796ea74f284e2c8dbd7cf2e2
jxq <jxqhhh@gmail.com>
2022-12-06 21:01:10 +0800

    Add debug config json

--- main.py
+++ main.py
@@ -7,13 +7,15 @@ import os
 parser = argparse.ArgumentParser()
 parser.add_argument("--video_idx", type = int)
 parser.add_argument("--dir", type = str, default="./video")
+parser.add_argument("--width", type = int, default=320)
+parser.add_argument("--height", type = int, default=180)
 args = parser.parse_args()
 
 video_idx = str(args.video_idx)
 video_idx = "0"*(3-len(video_idx))+str(video_idx)
 
-width = 320
-height = 168
+width = args.width
+height = args.height
 nframes = 100
 
 motion = np.zeros((nframes, height, width, 2))
@@ -34,8 +36,8 @@ for frame_idx in range(nframes):
 		cur_x   = int(matches.group(7))
 		cur_y   = int(matches.group(8))
 		assert ref_poc < cur_poc
-		motion[frame_idx, cur_y - h//2 : cur_y + h//2, cur_x - w//2 : cur_x + w//2, 0] = (cur_x - ref_x) * 2 / (cur_poc - ref_poc)
-		motion[frame_idx, cur_y - h//2 : cur_y + h//2, cur_x - w//2 : cur_x + w//2, 1] = (cur_y - ref_y) * 2 / (cur_poc - ref_poc)
+		motion[frame_idx, max(0, cur_y - h//2) : min(cur_y + h//2, height), max(0, cur_x - w//2) : min(cur_x + w//2, width), 0] = (cur_x - ref_x) * 2 / (cur_poc - ref_poc)
+		motion[frame_idx, max(0, cur_y - h//2) : min(cur_y + h//2, height), max(0, cur_x - w//2) : min(cur_x + w//2, width), 1] = (cur_y - ref_y) * 2 / (cur_poc - ref_poc)
 		
 np.save(os.path.join(args.dir, os.path.join(video_idx, "motion.npy")), motion)
 		
